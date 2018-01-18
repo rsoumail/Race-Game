cd5f1c934ef24436dff86c729a5c905fe293bded
Soumaila Ramadan <ramadansum@gmail.com>
2018-01-18 17:51:50 +0100

    Add fixed cameras and Cameras management

--- main.3.js
+++ main.3.js
@@ -4,14 +4,15 @@
 
 //Loads all dependencies
 requirejs(['ModulesLoaderV2.js'], function()
-		{ 
+		{
 			// Level 0 includes
 			ModulesLoader.requireModules(["threejs/three.min.js"]) ;
-			ModulesLoader.requireModules([ "myJS/ThreeRenderingEnv.js", 
-			                              "myJS/ThreeLightingEnv.js", 
-			                              "myJS/ThreeLoadingEnv.js", 
+			ModulesLoader.requireModules([ "myJS/ThreeRenderingEnv.js",
+			                              "myJS/ThreeLightingEnv.js",
+			                              "myJS/ThreeLoadingEnv.js",
 			                              "myJS/navZ.js",
-			                              "FlyingVehicle.js"]) ;
+			                              "FlyingVehicle.js",
+                                  "myJS/CameraManagement.js"]) ;
 			// Loads modules contained in includes and starts main function
 			ModulesLoader.loadModules(start) ;
 		}
@@ -24,17 +25,19 @@ function start()
 	//	author(s) : Cozot, R. and Lamarche, F.
 	//	date : 11/16/2014
 	//	last : 11/25/2014
-	//	---------------------------------------------------------------------------- 			
+	//	----------------------------------------------------------------------------
 	//	global vars
 	//	----------------------------------------------------------------------------
 	//	keyPressed
 	var currentlyPressedKeys = {};
-	
+
+  var fixed = true;
+
 	// car Position
-	var CARx = -220; 
-	var CARy = 0 ; 
+	var CARx = -220;
+	var CARy = 0 ;
 	var CARz = 0 ;
-	var CARtheta = 0 ; 
+	var CARtheta = 0 ;
 
 	// Creates the vehicle (handled by physics)
 	var vehicle = new FlyingVehicle(
@@ -43,7 +46,7 @@ function start()
 				zAngle : CARtheta+Math.PI/2.0,
 			}
 			) ;
-	
+
 	//	rendering env
 	var renderingEnvironment =  new ThreeRenderingEnv();
 
@@ -59,73 +62,79 @@ function start()
 	Loader.loadMesh('assets','circuit_Zup_02','obj',renderingEnvironment.scene,'circuit',	-340,-340,0,'front');
 	//Loader.loadMesh('assets','tree_Zup_02','obj',	renderingEnvironment.scene,'trees',	-340,-340,0,'double');
 	Loader.loadMesh('assets','arrivee_Zup_01','obj',	renderingEnvironment.scene,'decors',	-340,-340,0,'front');
-		
+
 	//	Car
 	// car Translation
-	var carPosition = new THREE.Object3D(); 
-	carPosition.name = 'car0'; 
-	renderingEnvironment.addToScene(carPosition); 
+	var carPosition = new THREE.Object3D();
+	carPosition.name = 'car0';
+	renderingEnvironment.addToScene(carPosition);
 	// initial POS
 	carPosition.position.x = CARx;
 	carPosition.position.y = CARy;
 	carPosition.position.z = CARz;
 	// car Rotation floor slope follow
-	var carFloorSlope = new THREE.Object3D(); 
+	var carFloorSlope = new THREE.Object3D();
 	carFloorSlope.name = 'car1';
 	carPosition.add(carFloorSlope);
 	// car vertical rotation
-	var carRotationZ = new THREE.Object3D(); 
+	var carRotationZ = new THREE.Object3D();
 	carRotationZ.name = 'car2';
 	carFloorSlope.add(carRotationZ);
 	carRotationZ.rotation.z = CARtheta ;
-	// the car itself 
+	// the car itself
 	// simple method to load an object
 	var carGeometry = Loader.load({filename: 'assets/car_Zup_01.obj', node: carRotationZ, name: 'car3'}) ;
+
 	carGeometry.position.z= +0.25 ;
 	// attach the scene camera to car
-//	carGeometry.add(renderingEnvironment.camera) ;
-//	renderingEnvironment.camera.position.x = 0.0 ;
-//	renderingEnvironment.camera.position.z = 10.0 ;
-//	renderingEnvironment.camera.position.y = -25.0 ;
-//	renderingEnvironment.camera.rotation.x = 85.0*3.14159/180.0 ;
-		
+  //carGeometry.add(renderingEnvironment.camera) ;
+	/*renderingEnvironment.camera.position.x = 0.0 ;
+	renderingEnvironment.camera.position.z = 10.0 ;
+	renderingEnvironment.camera.position.y = -25.0 ;
+	renderingEnvironment.camera.rotation.x = 85.0*3.14159/180.0 ;*/
+
 	//	Skybox
 	Loader.loadSkyBox('assets/maps',['px','nx','py','ny','pz','nz'],'jpg', renderingEnvironment.scene, 'sky',4000);
 
-	//	Planes Set for Navigation 
-	// 	z up 
+  var cameraManagement = new CameraManagement();
+
+  // positions des cameras fixes
+
+
+	//	Planes Set for Navigation
+	// 	z up
 	var NAV = new navPlaneSet(
-					new navPlane('p01',	-260, -180,	 -80, 120,	+0,+0,'px')); 		// 01	
-	NAV.addPlane(	new navPlane('p02', -260, -180,	 120, 200,	+0,+20,'py')); 		// 02		
-	NAV.addPlane(	new navPlane('p03', -260, -240,	 200, 240,	+20,+20,'px')); 	// 03		
-	NAV.addPlane(	new navPlane('p04', -240, -160,  200, 260,	+20,+20,'px')); 	// 04		
-	NAV.addPlane(	new navPlane('p05', -160,  -80,  200, 260,	+20,+40,'px')); 	// 05		
-	NAV.addPlane(	new navPlane('p06',  -80, -20,   200, 260,	+40,+60,'px')); 	// 06		
-	NAV.addPlane(	new navPlane('p07',  -20,  +40,  140, 260,	+60,+60,'px')); 	// 07		
-	NAV.addPlane(	new navPlane('p08',    0,  +80,  100, 140,	+60,+60,'px')); 	// 08		
-	NAV.addPlane(	new navPlane('p09',   20, +100,   60, 100,	+60,+60,'px')); 	// 09		
-	NAV.addPlane(	new navPlane('p10',   40, +100,   40,  60,	+60,+60,'px')); 	// 10		
-	NAV.addPlane(	new navPlane('p11',  100,  180,   40, 100,	+40,+60,'nx')); 	// 11		
-	NAV.addPlane(	new navPlane('p12',  180,  240,   40,  80,	+40,+40,'px')); 	// 12		
-	NAV.addPlane(	new navPlane('p13',  180,  240,    0,  40,	+20,+40,'py')); 	// 13 		
-	NAV.addPlane(	new navPlane('p14',  200,  260,  -80,   0,	+0,+20,'py')); 		// 14		
-	NAV.addPlane(	new navPlane('p15',  180,  240, -160, -80,	+0,+40,'ny')); 		// 15		
-	NAV.addPlane(	new navPlane('p16',  160,  220, -220,-160,	+40,+40,'px')); 	// 16	
-	NAV.addPlane(	new navPlane('p17',   80,  160, -240,-180,	+40,+40,'px')); 	// 17	
-	NAV.addPlane(	new navPlane('p18',   20,   80, -220,-180,	+40,+40,'px')); 	// 18	
-	NAV.addPlane(	new navPlane('p19',   20,   80, -180,-140,	+40,+60,'py')); 	// 19	
-	NAV.addPlane(	new navPlane('p20',   20,   80, -140,-100,	+60,+80,'py')); 	// 20	
-	NAV.addPlane(	new navPlane('p21',   20,   60, -100, -40,	+80,+80,'px')); 	// 21		
-	NAV.addPlane(	new navPlane('p22',  -80,   20, -100, -40,	+80,+80,'px')); 	// 22		
-	NAV.addPlane(	new navPlane('p23', -140,  -80, -100, -40,	+80,+80,'px')); 	// 23		
-	NAV.addPlane(	new navPlane('p24', -140,  -80, -140,-100,	+60,+80,'py')); 	// 24		
-	NAV.addPlane(	new navPlane('p25', -140,  -80, -200,-140,	+40,+60,'py')); 	// 25		
-	NAV.addPlane(	new navPlane('p26', -100,  -80, -240,-200,	+40,+40,'px')); 	// 26		
-	NAV.addPlane(	new navPlane('p27', -220, -100, -260,-200,	+40,+40,'px')); 	// 27	
-	NAV.addPlane(	new navPlane('p28', -240, -220, -240,-200,	+40,+40,'px')); 	// 28	
-	NAV.addPlane(	new navPlane('p29', -240, -180, -200,-140,	+20,+40,'ny')); 	// 29	
-	NAV.addPlane(	new navPlane('p30', -240, -180, -140, -80,	+0,+20,'ny')); 		// 30			
-	NAV.setPos(CARx,CARy,CARz); 
+					new navPlane('p01',	-260, -180,	 -80, 120,	+0,+0,'px')); 		// 01
+	NAV.addPlane(	new navPlane('p02', -260, -180,	 120, 200,	+0,+20,'py')); 		// 02
+	NAV.addPlane(	new navPlane('p03', -260, -240,	 200, 240,	+20,+20,'px')); 	// 03
+	NAV.addPlane(	new navPlane('p04', -240, -160,  200, 260,	+20,+20,'px')); 	// 04
+	NAV.addPlane(	new navPlane('p05', -160,  -80,  200, 260,	+20,+40,'px')); 	// 05
+	NAV.addPlane(	new navPlane('p06',  -80, -20,   200, 260,	+40,+60,'px')); 	// 06
+	NAV.addPlane(	new navPlane('p07',  -20,  +40,  140, 260,	+60,+60,'px')); 	// 07
+	NAV.addPlane(	new navPlane('p08',    0,  +80,  100, 140,	+60,+60,'px')); 	// 08
+	NAV.addPlane(	new navPlane('p09',   20, +100,   60, 100,	+60,+60,'px')); 	// 09
+	NAV.addPlane(	new navPlane('p10',   40, +100,   40,  60,	+60,+60,'px')); 	// 10
+	NAV.addPlane(	new navPlane('p11',  100,  180,   40, 100,	+40,+60,'nx')); 	// 11
+	NAV.addPlane(	new navPlane('p12',  180,  240,   40,  80,	+40,+40,'px')); 	// 12
+	NAV.addPlane(	new navPlane('p13',  180,  240,    0,  40,	+20,+40,'py')); 	// 13
+	NAV.addPlane(	new navPlane('p14',  200,  260,  -80,   0,	+0,+20,'py')); 		// 14
+	NAV.addPlane(	new navPlane('p15',  180,  240, -160, -80,	+0,+40,'ny')); 		// 15
+	NAV.addPlane(	new navPlane('p16',  160,  220, -220,-160,	+40,+40,'px')); 	// 16
+	NAV.addPlane(	new navPlane('p17',   80,  160, -240,-180,	+40,+40,'px')); 	// 17
+	NAV.addPlane(	new navPlane('p18',   20,   80, -220,-180,	+40,+40,'px')); 	// 18
+	NAV.addPlane(	new navPlane('p19',   20,   80, -180,-140,	+40,+60,'py')); 	// 19
+	NAV.addPlane(	new navPlane('p20',   20,   80, -140,-100,	+60,+80,'py')); 	// 20
+	NAV.addPlane(	new navPlane('p21',   20,   60, -100, -40,	+80,+80,'px')); 	// 21
+	NAV.addPlane(	new navPlane('p22',  -80,   20, -100, -40,	+80,+80,'px')); 	// 22
+	NAV.addPlane(	new navPlane('p23', -140,  -80, -100, -40,	+80,+80,'px')); 	// 23
+	NAV.addPlane(	new navPlane('p24', -140,  -80, -140,-100,	+60,+80,'py')); 	// 24
+	NAV.addPlane(	new navPlane('p25', -140,  -80, -200,-140,	+40,+60,'py')); 	// 25
+	NAV.addPlane(	new navPlane('p26', -100,  -80, -240,-200,	+40,+40,'px')); 	// 26
+	NAV.addPlane(	new navPlane('p27', -220, -100, -260,-200,	+40,+40,'px')); 	// 27
+	NAV.addPlane(	new navPlane('p28', -240, -220, -240,-200,	+40,+40,'px')); 	// 28
+	NAV.addPlane(	new navPlane('p29', -240, -180, -200,-140,	+20,+40,'ny')); 	// 29
+	NAV.addPlane(	new navPlane('p30', -240, -180, -140, -80,	+0,+20,'ny')); 		// 30
+	NAV.setPos(CARx,CARy,CARz);
 	NAV.initActive();
 	// DEBUG
 	//NAV.debug();
@@ -135,9 +144,9 @@ function start()
 	//	---------------------------------------------------------------------------
 	//	resize window
 	window.addEventListener( 'resize', onWindowResize, false );
-	//	keyboard callbacks 
+	//	keyboard callbacks
 	document.onkeydown = handleKeyDown;
-	document.onkeyup = handleKeyUp;					
+	document.onkeyup = handleKeyUp;
 
 	//	callback functions
 	//	---------------------------------------------------------------------------
@@ -151,38 +160,41 @@ function start()
 			renderingEnvironment.scene.traverse(function(o){
 				console.log('object:'+o.name+'>'+o.id+'::'+o.type);
 			});
-		}				
+		}
+    if (currentlyPressedKeys[80]) {
+      fixed = !fixed;
+    }
 		if (currentlyPressedKeys[68]) // (D) Right
 		{
 			vehicle.turnRight(1000) ;
 		}
-		if (currentlyPressedKeys[81]) // (Q) Left 
-		{		
+		if (currentlyPressedKeys[81]) // (Q) Left
+		{
 			vehicle.turnLeft(1000) ;
 		}
 		if (currentlyPressedKeys[90]) // (Z) Up
 		{
 			vehicle.goFront(1200, 1200) ;
 		}
-		if (currentlyPressedKeys[83]) // (S) Down 
+		if (currentlyPressedKeys[83]) // (S) Down
 		{
 			vehicle.brake(100) ;
 		}
 	}
 
 	//	window resize
-	function  onWindowResize() 
+	function  onWindowResize()
 	{
 		renderingEnvironment.onWindowResize(window.innerWidth,window.innerHeight);
 	}
 
-	function render() { 
+	function render() {
 		requestAnimationFrame( render );
 		handleKeys();
-		// Vehicle stabilization 
-		vehicle.goUp(vehicle.weight()/4.0, vehicle.weight()/4.0, vehicle.weight()/4.0, vehicle.weight()/4.0) ;
-		vehicle.stopAngularSpeedsXY() ;
-		vehicle.stabilizeSkid(50) ; 
+		// Vehicle stabilization
+		//vehicle.goUp(vehicle.weight()/4.0, vehicle.weight()/4.0, vehicle.weight()/4.0, vehicle.weight()/4.0) ;
+		//vehicle.stopAngularSpeedsXY() ;
+		vehicle.stabilizeSkid(50) ;
 		vehicle.stabilizeTurn(1000) ;
 		var oldPosition = vehicle.position.clone() ;
 		vehicle.update(1.0/60) ;
@@ -196,19 +208,22 @@ function start()
 		vehicle.position.x = NAV.x ;
 		vehicle.position.y = NAV.Y ;
 		// Updates carFloorSlope
-		carFloorSlope.matrixAutoUpdate = false;		
+		carFloorSlope.matrixAutoUpdate = false;
 		carFloorSlope.matrix.copy(NAV.localMatrix(CARx,CARy));
 		// Updates carRotationZ
 		carRotationZ.rotation.z = vehicle.angles.z-Math.PI/2.0 ;
+
 		// Camera
-		renderingEnvironment.camera.position.x = NAV.x ;
+		/*renderingEnvironment.camera.position.x = NAV.x ;
 		renderingEnvironment.camera.position.y = NAV.y ;
 		renderingEnvironment.camera.position.z = NAV.z+50+vehicle.speed.length()*2 ;
-		console.log(vehicle.speed.z) ;
-//		renderingEnvironment.camera.rotation.z = vehicle.angles.z-Math.PI/2.0 ;
-		// Rendering
-		renderingEnvironment.renderer.render(renderingEnvironment.scene, renderingEnvironment.camera); 
-	};
+		/*console.log(vehicle.speed.z) ;*/
 
-	render(); 
+
+
+  renderingEnvironment.renderer.render(renderingEnvironment.scene, cameraManagement.switchCamera(fixed, NAV, carPosition, carGeometry, renderingEnvironment))
+
+};
+
+	render();
 }
---
+++ myJS/CameraManagement.js
@@ -0,0 +1,87 @@
+
+
+
+CameraManagement = function(){
+
+
+  this.camerasPositions = [
+    {
+      camera: new THREE.Vector3(-260, -160, 100),
+      positions: ['p01', 'p02', 'p03']
+    },
+    {
+      camera: new THREE.Vector3(-240, 260, 100),
+      positions: ['p04', 'p05', 'p06']
+    },
+    {
+      camera: new THREE.Vector3(40, 260, 100),
+      positions: ['p07','p08']
+    },
+    {
+      camera: new THREE.Vector3(20, 60, 100),
+      positions: ['p09', 'p11', 'p10']
+    },
+    {
+      camera: new THREE.Vector3(240, 80, 100),
+      positions: ['p12', 'p13','p14']
+    },
+    {
+      camera: new THREE.Vector3(180, -80, 100),
+      positions: ['p15', 'p16']
+    },
+    {
+      camera: new THREE.Vector3(220, -220, 100),
+      positions: ['p17', 'p18']
+    },
+    {
+      camera: new THREE.Vector3(20, -180, 100),
+      positions: ['p19', 'p20']
+    },
+    {
+      camera: new THREE.Vector3(60, -40, 100),
+      positions: ['p21', 'p22', 'p23']
+    },
+    {
+      camera: new THREE.Vector3(-140, -100, 100),
+      positions: ['p23','p24', 'p25']
+    },
+    {
+      camera: new THREE.Vector3(-100, -240, 100),
+      positions: ['p26', 'p27', 'p28']
+    },
+    {
+      camera: new THREE.Vector3(-240, -240, 100),
+      positions: ['p29', 'p30']
+    }
+  ]
+
+  this.switchCamera = function(type, NAV, carPosition, carGeometry, renderingEnvironment){
+    if(type){
+      var active = NAV.findActive(carPosition.position.x, carPosition.position.y);
+      var plane = NAV.planeSet[active];
+      var newPosition
+      this.camerasPositions.forEach((c) => {
+        c.positions.forEach((p) => {
+          if(plane.name === p){
+            newPosition = c.camera
+            return true
+          }
+        });
+      });
+      carGeometry.remove(renderingEnvironment.camera) ;
+      renderingEnvironment.camera.position.x = newPosition.x
+      renderingEnvironment.camera.position.y = newPosition.y
+      renderingEnvironment.camera.position.z = newPosition.z
+      //renderingEnvironment.camera.rotation.x = vehicle.angles.z-Math.PI/2.0;
+      renderingEnvironment.camera.up = new THREE.Vector3(0,  0, 1);
+      renderingEnvironment.camera.lookAt(new THREE.Vector3(carPosition.position.x, carPosition.position.y, carPosition.position.z));
+    } else {
+      carGeometry.add(renderingEnvironment.camera) ;
+    	renderingEnvironment.camera.position.x = 0.0 ;
+    	renderingEnvironment.camera.position.z = 10.0 ;
+    	renderingEnvironment.camera.position.y = -25.0 ;
+    	renderingEnvironment.camera.rotation.x = 85.0*3.14159/180.0 ;
+    }
+    return renderingEnvironment.camera
+  }
+}
